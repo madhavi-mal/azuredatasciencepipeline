4525a9286c405fd777e6bad4005aac24cd74bc37
Madhavi Mallela <mamalle@microsoft.com>
2019-09-29 20:20:57 +0000

    Adding clouddrive files

---
+++ install-adb-spark-cluster.sh
@@ -0,0 +1,8 @@
+pip install databricks-cli
+databricks configure --token
+#add multiple connection profiles
+databricks configure --profile madhaviadb1
+databricks workspace ls --profile madhaviadb1
+
+
+
